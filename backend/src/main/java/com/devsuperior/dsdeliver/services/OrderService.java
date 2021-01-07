564b4123baffbb9352f27b7598a38fbb4f6c82ba
luanalinsv@gmail.com <luanalinsv@gmail.com>
2021-01-07 12:13:53 -0400

    PUT/ orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -49,7 +49,16 @@ public class OrderService {
 		return new OrderDTO(order);
 		
 	}
+
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		return new OrderDTO(order);
+	}
+
 }
+
 
 
 
