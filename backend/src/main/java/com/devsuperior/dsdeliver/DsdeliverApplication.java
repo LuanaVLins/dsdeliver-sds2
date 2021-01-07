0ce49f46f363dde567d03d56795951e249119ee5
luanalinsv@gmail.com <luanalinsv@gmail.com>
2021-01-07 09:14:25 -0400

    Bean error

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -2,8 +2,11 @@ package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = 
+{DataSourceAutoConfiguration.class
+		} )
 public class DsdeliverApplication {
 
 	public static void main(String[] args) {
