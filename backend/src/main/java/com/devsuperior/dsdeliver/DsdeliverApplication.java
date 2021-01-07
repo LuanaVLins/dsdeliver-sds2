a3b94d7cf639dd2f092ec848b6bd5a7a7fe62552
luanalinsv@gmail.com <luanalinsv@gmail.com>
2021-01-07 09:46:49 -0400

    GET /products

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -2,11 +2,9 @@ package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = 
-{DataSourceAutoConfiguration.class
-		} )
+
+@SpringBootApplication
 public class DsdeliverApplication {
 
 	public static void main(String[] args) {
