a7beb4de27a6d3c08d372c0da97e857479fce60b
luanalinsv@gmail.com <luanalinsv@gmail.com>
2021-01-09 08:58:53 -0400

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,14 +1,16 @@
 import'./styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
 function Home(){
     return (
-       <div className="home-container">
+       <>
+        <div className="home-container">
            <div className="home-content">
                <div className="home-actions">
                     <h1 className="home-title">
                         Faça seu pedido <br /> que entregamos <br /> pra você!!!
-                     </h1>
+                    </h1>
                      <h3 className="home-subtitle">
                      Escolha o seu pedido e em poucos minutos <br /> levaremos na sua porta
                      </h3>
@@ -21,6 +23,8 @@ function Home(){
                 </div>
            </div>
        </div>
+        <Footer />
+       </>
     )
 }
 
